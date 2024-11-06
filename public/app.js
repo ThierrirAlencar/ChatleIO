3acf3c3f6bdbd611ea6f0034995f0fa2892e9275
ciringa <thierrird957468@gmail.com>
2024-11-06 10:44:02 -0300

    socket.io:setup

--- public/app.js
+++ public/app.js
@@ -1,11 +1,26 @@
-const socket = new WebSocket("ws://localhost:8080");
+// const socket = new WebSocket("ws://localhost:8080");
 
-//listen for messages 
-socket.onmessage = ({data})=>{
-    console.log("message from the server", data)
-}
+// //listen for messages 
+// socket.onmessage = ({data})=>{
+//     console.log("message from the server", data)
+// }
 
-//send a message
+// //send a message
+// document.querySelector("button").addEventListener("click",()=>{
+//     socket.send("Hello World")
+// })
+
+const socket = io("ws://localhost:8080");
+
+//listen to message event
+socket.on("message",(text)=>{
+    const el = document.createElement("p");
+    el.innerHTML = text;
+    document.querySelector(".messageSpace").appendChild(el);
+})
+
 document.querySelector("button").addEventListener("click",()=>{
-    socket.send("Hello World")
+    const content = document.querySelector("input").value
+    //refers to wich part we want to use
+    socket.emit("message",content)
 })
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,36 @@
-import WebScoket from "ws"
+import fastify from "fastify";
+import { createServer } from "http";
+import { Server } from 'socket.io';
+ 
+const app = fastify();
+const server = createServer(app.server)
+
+const io =  new Server(server,{
+    cors:{
+        origin:"*" //alow any url to access our api
+    }
+})
+
+//main event (everyone will use this to connect with us)
+io.on("connection",(socket)=>{
+    console.log("New client connected!");
+
+    socket.on("message",(message)=>{
+        console.log("message event called")
+        io.emit("message",`${socket.id.substr(0,2)} said ${message}`)
+    })
+})
+
+//common backend
+app.get("/",async(req,res)=>{
+    res.send({
+        message:"Bem vindo ao server de websocket"
+    })
+})
+
 
 const port = 8080
 
-//initialize a new WebSocket server
-const server = new WebScoket.Server({port});
-
-
-server.on('connection',(socket)=>{
-    
-    console.log(`server running at ${port}`)
-
-    socket.on("message",(message)=>{
-        socket.send(message)
-    })
+server.listen(port,()=>{
+    console.log("server running at:"+port)
 })
